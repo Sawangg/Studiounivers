5ceb97d4713f5295a57860bfa380d07599975b40
Sawangg <38500427+Sawangg@users.noreply.github.com>
2022-05-31 11:19:47 +0200

    Continued setup of project

---
+++ frontend/modules/landing/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Navbar: React.FC<{}> = () => (
+    <nav className="fixed w-full z-30 top-0 text-primary-700 shadow-md bg-background-100">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+            <div className="pl-4 flex items-center">
+                logo
+            </div>
+            <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-background-100 lg:bg-transparent p-4 lg:p-0 z-20">
+                <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                    <li className="mr-5">
+                        Register
+                    </li>
+                    <li className="mr-5">
+                        Login
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { Navbar } from "../modules/landing/Navbar";
 
-const Home: NextPage = () => {
-    return (
+const Landing: NextPage = () => (
+    <>
+        <Head>
+            <title>StudioUnivers</title>
+        </Head>
         <div className="bg-blue w-1 h-1">
-            <h1 className="text-3xl font-bold underline">t</h1>
+            <Navbar />
         </div>
-    )
-};
+    </>
+);
 
-export default Home;
+export default Landing;
