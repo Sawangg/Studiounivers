f94316a9571a90ec83e138ec5018a598ff443174
Sawangg <38500427+Sawangg@users.noreply.github.com>
2022-08-08 14:39:22 +0200

    Updated fonts and packages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,7 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-};
-
-module.exports = {
-    nextConfig,
     i18n,
 };
+
+module.exports = nextConfig;
